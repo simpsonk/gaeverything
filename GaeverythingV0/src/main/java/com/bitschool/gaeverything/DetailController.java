3f528c61b6147544c32acd37536662d6ef74a22d
DESKTOP-DOGVD1T\BIT <countdastars@naver.com>
2017-07-21 11:44:12 +0900

    DetailController 추가

---
+++ GaeverythingV0/src/main/java/com/bitschool/gaeverything/DetailController.java
@@ -0,0 +1,12 @@
+package com.bitschool.gaeverything;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+@RequestMapping(value = "map/detail")
+@Controller
+public class DetailController {
+
+
+	
+	
+}
