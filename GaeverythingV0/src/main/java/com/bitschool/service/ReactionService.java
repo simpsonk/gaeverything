d4b1f5604ca3ff905d34eb7df2d6349a183a44a4
유진 <leeyjeen@naver.com>
2017-08-11 17:25:47 +0900

    리뷰게시판에 댓글 달 때 reaction 테이블에 저장되도록 함

---
+++ GaeverythingV0/src/main/java/com/bitschool/service/ReactionService.java
@@ -0,0 +1,32 @@
+package com.bitschool.service;
+
+import java.sql.SQLException;
+import java.util.List;
+
+import javax.inject.Inject;
+
+import org.springframework.stereotype.Service;
+
+import com.bitschool.dao.ReactionDAO;
+import com.bitschool.dto.ReactionDTO;
+
+
+@Service
+public class ReactionService {
+	
+	@Inject
+	private ReactionDAO dao;
+	
+	public boolean insertReaction(ReactionDTO dto){
+		boolean flag = false;
+		try {
+			flag = dao.insertReaction(dto);
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		return flag;
+	}
+	
+
+}
