c434d967947a83d0f92141af222ff59fe1cd2d04
유진 <leeyjeen@naver.com>
2017-08-21 15:55:07 +0900

    회원레벨링관련 추가 + 회원가입할때 grade 테이블에 default값으로 insert기능 추가

---
+++ GaeverythingV0/src/main/java/com/bitschool/dao/GradeDAO.java
@@ -0,0 +1,30 @@
+package com.bitschool.dao;
+
+
+import java.sql.SQLException;
+
+import javax.inject.Inject;
+
+import org.apache.ibatis.session.SqlSession;
+import org.springframework.stereotype.Repository;
+
+import com.bitschool.dto.GradeDTO;
+
+@Repository
+public class GradeDAO {
+
+	@Inject
+	private SqlSession session;
+	
+	private static final String namespace = "com.bitschool.gaverything.GradeMapper";
+
+	// 회원가입할 때 호출한다
+	public boolean insertNew(GradeDTO dto) throws SQLException{
+		boolean flag = false;
+		int aCnt = session.insert(namespace+".insertNew",dto);
+		if(aCnt>0){
+			flag = true;
+		}
+		return flag;
+	}
+}
---
+++ GaeverythingV0/src/main/java/com/bitschool/dto/GradeDTO.java
@@ -0,0 +1,90 @@
+package com.bitschool.dto;
+
+public class GradeDTO {
+	private int gradeSeq;
+	private String nickname;
+	private String resComment;
+	private String resBookmark;
+	private String myReview;
+	private String myComment;
+	private String myBookmark;
+	private int point;
+	private String grade;
+	
+	public GradeDTO(){
+		
+	}
+	
+	public GradeDTO(String nickname){
+		this.nickname = nickname;
+	}
+	
+	public int getGradeSeq() {
+		return gradeSeq;
+	}
+	public void setGradeSeq(int gradeSeq) {
+		this.gradeSeq = gradeSeq;
+	}
+	public String getNickname() {
+		return nickname;
+	}
+	public void setNickname(String nickname) {
+		this.nickname = nickname;
+	}
+	public String getResComment() {
+		return resComment;
+	}
+	public void setResComment(String resComment) {
+		this.resComment = resComment;
+	}
+	public String getResBookmark() {
+		return resBookmark;
+	}
+	public void setResBookmark(String resBookmark) {
+		this.resBookmark = resBookmark;
+	}
+	public String getMyReview() {
+		return myReview;
+	}
+	public void setMyReview(String myReview) {
+		this.myReview = myReview;
+	}
+	public String getMyComment() {
+		return myComment;
+	}
+	public void setMyComment(String myComment) {
+		this.myComment = myComment;
+	}
+	public String getMyBookmark() {
+		return myBookmark;
+	}
+	public void setMyBookmark(String myBookmark) {
+		this.myBookmark = myBookmark;
+	}
+	public int getPoint() {
+		return point;
+	}
+	public void setPoint(int point) {
+		this.point = point;
+	}
+	public String getGrade() {
+		return grade;
+	}
+	public void setGrade(String grade) {
+		this.grade = grade;
+	}
+	
+	@Override
+	public String toString() {
+		return "GradeDTO [gradeSeq=" + gradeSeq + ", nickname=" + nickname + ", resComment=" + resComment
+				+ ", resBookmark=" + resBookmark + ", myReview=" + myReview + ", myComment=" + myComment
+				+ ", myBookmark=" + myBookmark + ", point=" + point + ", grade=" + grade + ", getGradeSeq()="
+				+ getGradeSeq() + ", getNickname()=" + getNickname() + ", getResComment()=" + getResComment()
+				+ ", getResBookmark()=" + getResBookmark() + ", getMyReview()=" + getMyReview() + ", getMyComment()="
+				+ getMyComment() + ", getMyBookmark()=" + getMyBookmark() + ", getPoint()=" + getPoint()
+				+ ", getGrade()=" + getGrade() + ", getClass()=" + getClass() + ", hashCode()=" + hashCode()
+				+ ", toString()=" + super.toString() + "]";
+	}
+	
+	
+}
